50362345ed6fe8b94e93f29ac85ba9576b18e3a2
Mijanur Rahman <mijan.connect@gmail.com>
2025-12-18 18:20:20 +0600

    Vite config changed

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,28 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,     // allows access from outside (e.g., EC2, domain)
+//     port: 5175,     // change this to your desired port
+//     allowedHosts: ['juror.glassfile.xyz'], // optional: prevent "host not allowed" errors
+//   },
+// })
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,     // allows access from outside (e.g., EC2, domain)
-    port: 5175,     // change this to your desired port
-    allowedHosts: ['juror.glassfile.xyz'], // optional: prevent "host not allowed" errors
+    host: true, // allows access from outside (e.g., EC2, domain)
+    port: 5175, // dev server port
+    allowedHosts: ["juror.glassfile.xyz"], // dev server
   },
-})
+  preview: {
+    host: true, // allow preview access externally
+    port: 4174, // optional, default is 4173
+    allowedHosts: ["juror.glassfile.xyz"], // must add host here for preview
+  },
+});
 
 // export default defineConfig({
 //   plugins: [react()],
@@ -18,4 +32,3 @@ export default defineConfig({
 //     port: 3016,
 //   },
 // });
-
